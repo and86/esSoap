cd1f98ec2aa8b853bee56835b99a25d1f56309b0
and86 <andrea.ln@tiscali.it>
2016-11-15 16:33:41 +0100

    client

---
+++ soapClient/src/it/alfasoft/andrea/prova.java
@@ -0,0 +1,29 @@
+package it.alfasoft.andrea;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+public class prova {
+
+	public static void main(String[] args){
+		
+		CalcolatriceServiceLocator locator=new CalcolatriceServiceLocator();
+		
+		try{
+			
+			Calcolatrice c=locator.getCalcolatrice();
+			
+			int res=c.add(4, 8);
+			System.out.println(res);
+		
+		} catch(ServiceException e){
+			e.printStackTrace();
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
